77d3dd6cb91c99f67bbf7c984a67d778f0bb6c36
Khales Rahman <106184751+mkhales30@users.noreply.github.com>
2022-10-27 17:19:46 -0400

    Update Poem.java

--- Poem.java
+++ Poem.java
@@ -1,46 +1,61 @@
 /**
- * class name: Poem
- * author: Monauver Rahman
- * course: ITEC 2150
- * date: October 26, 2022
- * version 1.0
- * description: A class representing information about a poem for use in Chapter 5 Exercise 2
+ * Poem.java
  *
-  */
+ * A class representing information about a poem for use in Chapter 5 Exercise 2
+ *
+ */
 public class Poem
 {
-    private String name;
-    private String poet;
-  
-    public Poem()
-    {
-        name = "unknown";
-        poet = "unknown";
-    }
-    public String getName()
-    {
-        return name;
-    }
-  
-    public void setName(String name)
-    {
-        this.name = name;
-    }
 
-    public String getPoet()
-    {
-        return poet;
-    }
+	private String name;
+	private String poet;
 
-    public void setPoet(String poet)
-    {
-        this.poet = poet;
-    }
+	/**
+	 * no-arg constructor
+	 */
+	public Poem()
+	{
+		// initialize attributes
+		name = "unknown";
+		poet = "unknown";
+	}
 
-    @Override
-    public String toString()
-    {
-        return "Poem [name=" + name + ", poet=" + poet + "]";
-    }
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	/**
+	 * @return the poet
+	 */
+	public String getPoet()
+	{
+		return poet;
+	}
+
+	/**
+	 * @param poet the poet to set
+	 */
+	public void setPoet(String poet)
+	{
+		this.poet = poet;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Poem [name=" + name + ", poet=" + poet + "]";
+	}
 
 }
